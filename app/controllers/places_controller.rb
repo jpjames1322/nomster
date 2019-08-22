47b58b5b495576ee0222c8c31b5be1991a3f16ee
jared-phillips <jp.james0927@gmail.com>
2019-08-22 01:22:06 +0000

    new

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,9 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+  def index
+  @places = Place.order("name").paginate(page: params[:page])
+  end
+  def new
+  end
 end
