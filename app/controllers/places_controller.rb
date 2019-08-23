4abfbcc43139c4123bdb57e7030b1f76e6048b1d
jared-phillips <jp.james0927@gmail.com>
2019-08-23 00:33:15 +0000

    create new place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,4 +8,11 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
+  def create
+    Place.create(place_params)
+  end
+  private
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
