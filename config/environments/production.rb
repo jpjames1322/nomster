744f1381aea1cc24d308b38bb9426ad1686ebd15
jared-phillips <jp.james0927@gmail.com>
2019-08-25 01:30:37 +0000

    login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
-Rails.application.configure do
+Rails.applications.configure do
+config.action_mailer.default_url_options = { host: 'https://nomster-jared-phillips.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
