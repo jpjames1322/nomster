d23088b7bfe6703a07d91a819bc6b9ed6cf2f93c
jared-phillips <jp.james0927@gmail.com>
2019-08-26 02:15:35 +0000

    places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,6 +17,9 @@ class PlacesController < ApplicationController
   current_user.places.create(place_params)
   redirect_to root_path
 end
+def show 
+  @place = Places.find(params[:id])
+  end
 
   private
   def place_params
