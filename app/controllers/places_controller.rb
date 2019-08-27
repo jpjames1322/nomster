b6f505e86b0f7e601ea13213956aa94bd3022ce1
jared-phillips <jp.james0927@gmail.com>
2019-08-27 02:10:03 +0000

    destroy

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -30,6 +30,11 @@ def show
     @place.update_attributes(place_params)
     redirect_to root_path
   end
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to root_path
+  end
 
   private
   def place_params
