6387ec80459456b80b5ab871144804b6f17f4b31
jared-phillips <jp.james0927@gmail.com>
2019-09-03 13:20:35 +0000

    update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,47 +11,49 @@ class PlacesController < ApplicationController
   end
 
   def create
-      @place = current_user.places.create(place_params)
-    if @place.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
+end
 
   def show
     @place = Place.find(params[:id])
   end
 
   def edit
-     @place = Place.find(params[:id])
-    if @place.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
+    @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+end
+
+ def update
+  @place = Place.find(params[:id])
+
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
   end
 
-  def update
-    @place = Place.find(params[:id])
+  @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
 
-    if @places.user != current_useer
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-      @place.update_attributes(place_params)
-      if @place.valid?
-        redirect_to root_path
-       else
-        render :edit, status: :unprocessable_entity
-     end
-   end
+def destroy
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
 
-  def destroy
-    @place = Place.find(params[:id])
-    if @places.user != current_user
-      return render plain: 'Not Allowed', status: :forbidden
-    end
-      @place.destroy
-      redirect_to root_path
-    end
+  @place.destroy
+  redirect_to root_path
+end
 
   private
 
