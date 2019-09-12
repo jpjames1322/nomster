b52598ac30fa12eb98808ac0470de9216dd145ff
jared-phillips <jp.james0927@gmail.com>
2019-09-12 01:48:47 +0000

    email

---
+++ config/image.rb
@@ -0,0 +1,18 @@
+class Image 
+  attr_accessor :image 
+
+  def initialize(image)
+            @image = image 
+  end  
+  def output_image
+    image.each do |image| 
+     puts image
+   end
+  end
+end 
+
+ image = Image.new([[0, 0, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 0, 0, 1],
+                    [0, 0, 0, 0]])
+ image.output_image
