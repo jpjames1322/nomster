a4b90d5ed4d6666eb9f7617cb1335f9407331307
jared-phillips <jp.james0927@gmail.com>
2019-09-14 15:42:57 +0000

    lesson 42

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-    mail(to: "my@email.com",
-          subject: "a comment has been added to your place")
+  def comment_added(comment)
+  @place = comment.place
+  @place_owner = @place.user
+  mail(to: @place_owner.jp.james0927@gmail.com,
+       subject: "A comment has been added to your place")
   end
 end
