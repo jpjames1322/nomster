38a6c73d9ebcb226ef9305a03623644bab4bd4aa
jared-phillips <jp.james0927@gmail.com>
2019-09-15 18:51:16 +0000

    email config

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,7 @@ class NotificationMailer < ApplicationMailer
   def comment_added(comment)
   @place = comment.place
   @place_owner = @place.user
-   mail(to: @place_owner.jp.james0927@gmail.com, subject: "A comment has been added to #{@place.name}")
+   mail(to: @jp.james0927@gmail.com, subject: "A comment has been added to #{@place.name}")
   end
+end
 end
